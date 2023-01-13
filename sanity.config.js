76bd758215aebf41e71f199acc930cf4e915f70a
Racheal Pennell <racheal.larimer@gmail.com>
2023-01-12 17:38:06 -0800

    add Studio embed to /studio

--- sanity.config.js
+++ sanity.config.js
@@ -1,5 +1,3 @@
-import { theme } from 'https://themer.sanity.build/api/hues?default=069292&primary=09d1d2;300&transparent=395f5f;600&positive=43d675;300&caution=fbd024;200&lightest=fcfdfd&darkest=0d1515';
-
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
@@ -10,7 +8,6 @@ const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineConfig({
-  theme,
   basePath: '/studio',
   name: 'default',
   title: 'sandbox',
