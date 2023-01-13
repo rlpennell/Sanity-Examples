c78cb93aa5966ce90377f4f100fdbe034af51e68
Racheal Pennell <racheal.larimer@gmail.com>
2023-01-12 17:59:42 -0800

    add webhook validation

--- sanity.config.js
+++ sanity.config.js
@@ -3,11 +3,13 @@ import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { sandboxTypes } from './schemas/sandbox';
 import { singletonPlugin, structure } from './plugins/settings';
+import { theme } from './plugins/theme';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineConfig({
+  theme,
   basePath: '/studio',
   name: 'default',
   title: 'sandbox',
